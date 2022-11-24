73a91c4be06bb7f17213512e726415411014995e
imp0rtt <44298462+imp0rtt@users.noreply.github.com>
2022-11-24 14:15:44 +0500

    Fixed dug with time output
    New function for time output

--- TimeAndTemp/TimeAndTemp.ino
+++ TimeAndTemp/TimeAndTemp.ino
@@ -1,38 +1,37 @@
 #include <microDS3231.h>
 #include <LiquidCrystal.h>
-
 #include "DHT.h"
 
 #define DHTPIN 4
 #define DHTTYPE DHT11
-
 DHT dht(DHTPIN, DHTTYPE);
 
-
 MicroDS3231 rtc;
+
+// initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 5, 9, 8, 7, 6);
 
 void setup() {
   rtc.setTime(COMPILE_TIME);
   // set up the LCD's number of columns and rows
   lcd.begin(16, 2);
-  // Print a message to the LCD.
+  // set the cursor to column 3, line 0
   lcd.setCursor(3, 0);
-  lcd.print("Time and Temp");
+  // Print a message to the LCD.
+  lcd.print("imp0rtTech");
 
   dht.begin();
 }
+
 void loop() {
   float t = dht.readTemperature();
-
-  lcd.setCursor(11, 1);
+  // set the cursor to column 10, line 1
+  lcd.setCursor(10, 1);
   lcd.print(t);
 
-  lcd.setCursor(2, 1);
-
-  lcd.print(rtc.getHours());
-  lcd.print(":");
-  lcd.print(rtc.getMinutes());
-  lcd.print(":");
-  lcd.print(rtc.getSeconds());
+  // set the cursor to column 1, line 1
+  lcd.setCursor(1, 1);
+  //get time as string
+  String s = rtc.getTimeString();
+  lcd.print(s);
 }
